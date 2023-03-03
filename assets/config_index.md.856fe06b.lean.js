8c5e75823a0cd5d2ed0266e66ebdf608471403cb
sfxcode <sfxcode@users.noreply.github.com>
2023-03-03 13:44:10 +0000

    deploy: 41f54602d014f7a6aeb8d81dc26c0740e281c9cf

--- assets/config_index.md.856fe06b.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as n,c as a,a as o}from"./app.9db16413.js";const u=JSON.parse('{"title":"Module Configuration","description":"","frontmatter":{},"headers":[],"relativePath":"config/index.md"}'),l={name:"config/index.md"},p=o("",32),e=[p];function t(r,c,i,F,y,D){return n(),a("div",null,e)}const d=s(l,[["render",t]]);export{u as __pageData,d as default};
